6a8c57173e57da5a2ee264abbf922402b05f156e
fayzayaganangputra <fayzayaganangputra20@gmail.com>
2025-09-16 01:51:09 +0700

    fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/indositek/', // 👈 Wajib, biar asset gak error di GitHub Pages
+  base: './', // penting biar path asset relatif (tidak blank putih di hosting)
+  build: {
+    outDir: 'dist',
+  },
 })
